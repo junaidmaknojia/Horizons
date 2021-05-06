2705c9fc65d574c2f769d69fb0d9eaa3b2ff7a8a
junaid <junaidmaknojia786@yahoo.com>
2021-05-06 17:36:12 -0500

    searches routes done

--- backend/app/api/search_routes.py
+++ backend/app/api/search_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, session, request
-from app.models import User, db
+from app.models import User, TagCategory, Tag, Role, RoleCategory, Industry, db
 from app.forms import LoginForm
 from app.forms import SignUpForm
 from flask_login import current_user, login_user, logout_user, login_required
@@ -13,4 +13,15 @@ def get_tags():
 
 @search_routes.route("/industries")
 def get_industries():
-    pass
+    industries = TagCategory.query.all()
+    return {"industries": [tc.to_dict() for tc in industries]}
+
+@search_routes.route("/tagCategories")
+def get_tag_categories():
+    tagCats = TagCategory.query.all()
+    return {"tagCats": [tc.to_dict() for tc in tagCats]}
+
+@search_routes.route("/roleCategories")
+def get_role_categories():
+    roleCats = TagCategory.query.all()
+    return {"roleCats": [tc.to_dict() for tc in roleCats]}
