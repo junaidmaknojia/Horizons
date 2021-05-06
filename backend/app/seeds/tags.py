25704b1847fe4eaee68736b182411926eb7f2bba
junaid <junaidmaknojia786@yahoo.com>
2021-05-05 23:48:50 -0500

    tags done, starting category seeds

--- backend/app/seeds/tags.py
+++ backend/app/seeds/tags.py
@@ -8,52 +8,52 @@ from app.models import db, Tag, TagCategory
 
 
 def seed_tags():
-    {name: "Incorporation", categoryId: 1},
-    {name: "Branding", categoryId: 1},
-    {name: "Creating a MVP", categoryId: 1},
-    {name: "Business model", categoryId: 1},
-    {name: "Financial model", categoryId: 1},
-    {name: "Pitching", categoryId: 1},
-    {name: "Customer Acquisition", categoryId: 1},
-    {name: "Operations", categoryId: 1},
-    {name: "Growth", categoryId: 1},
-    {name: "Product feedback", categoryId: 1},
-    {name: "Product-market fit", categoryId: 1},
-    {name: "Business development", categoryId: 1},
-    {name: "Scaling a company", categoryId: 1},
-    {name: "Marketing", categoryId: 1},
-    {name: "Business Strategy", categoryId: 1},
-    {name: "Team building", categoryId: 1},
-    {name: "Legal", categoryId: 1},
-    {name: "Fundraising", categoryId: 1},
-    {name: "Upskilling", categoryId: 2},
-    {name: "Coding", categoryId: 2},
-    {name: "Law School application", categoryId: 2},
-    {name: "Business School application", categoryId: 2},
-    {name: "Grad school application", categoryId: 2},
-    {name: "Investing", categoryId: 2},
-    {name: "Digital skills training", categoryId: 2},
-    {name: "Getting an internship", categoryId: 3},
-    {name: "Getting first job", categoryId: 3},
-    {name: "Career advancement", categoryId: 3},
-    {name: "Setting professional goals", categoryId: 3},
-    {name: "Resume/job applications", categoryId: 3},
-    {name: "Getting a promotion", categoryId: 3},
-    {name: "Salary Negotiation", categoryId: 3},
-    {name: "Recruiting help", categoryId: 3},
-    {name: "Switching careers", categoryId: 3},
-    {name: "Confidence building", categoryId: 4},
-    {name: "Public speaking", categoryId: 4},
-    {name: "Job interview skills", categoryId: 4},
-    {name: "Negotiation strategies", categoryId: 4},
-    {name: "Goal setting", categoryId: 4},
-    {name: "Professional networking", categoryId: 4},
-    {name: "Leadership skills", categoryId: 4},
-    {name: "Getting on boards", categoryId: 4},
-    {name: "Communication skills", categoryId: 4},
-    {name: "Managing teams", categoryId: 4},
-    {name: "Building a personal brand", categoryId: 4},
-    {name: "Work/Life Balance", categoryId: 5},
+    db.session.add(Tag(name: "Incorporation", categoryId: 1))
+    db.session.add(Tag(name: "Branding", categoryId: 1))
+    db.session.add(Tag(name: "Creating a MVP", categoryId: 1))
+    db.session.add(Tag(name: "Business model", categoryId: 1))
+    db.session.add(Tag(name: "Financial model", categoryId: 1))
+    db.session.add(Tag(name: "Pitching", categoryId: 1))
+    db.session.add(Tag(name: "Customer Acquisition", categoryId: 1))
+    db.session.add(Tag(name: "Operations", categoryId: 1))
+    db.session.add(Tag(name: "Growth", categoryId: 1))
+    db.session.add(Tag(name: "Product feedback", categoryId: 1))
+    db.session.add(Tag(name: "Product-market fit", categoryId: 1))
+    db.session.add(Tag(name: "Business development", categoryId: 1))
+    db.session.add(Tag(name: "Scaling a company", categoryId: 1))
+    db.session.add(Tag(name: "Marketing", categoryId: 1))
+    db.session.add(Tag(name: "Business Strategy", categoryId: 1))
+    db.session.add(Tag(name: "Team building", categoryId: 1))
+    db.session.add(Tag(name: "Legal", categoryId: 1))
+    db.session.add(Tag(name: "Fundraising", categoryId: 1))
+    db.session.add(Tag(name: "Upskilling", categoryId: 2))
+    db.session.add(Tag(name: "Coding", categoryId: 2))
+    db.session.add(Tag(name: "Law School application", categoryId: 2))
+    db.session.add(Tag(name: "Business School application", categoryId: 2))
+    db.session.add(Tag(name: "Grad school application", categoryId: 2))
+    db.session.add(Tag(name: "Investing", categoryId: 2))
+    db.session.add(Tag(name: "Digital skills training", categoryId: 2))
+    db.session.add(Tag(name: "Getting an internship", categoryId: 3))
+    db.session.add(Tag(name: "Getting first job", categoryId: 3))
+    db.session.add(Tag(name: "Career advancement", categoryId: 3))
+    db.session.add(Tag(name: "Setting professional goals", categoryId: 3))
+    db.session.add(Tag(name: "Resume/job applications", categoryId: 3))
+    db.session.add(Tag(name: "Getting a promotion", categoryId: 3))
+    db.session.add(Tag(name: "Salary Negotiation", categoryId: 3))
+    db.session.add(Tag(name: "Recruiting help", categoryId: 3))
+    db.session.add(Tag(name: "Switching careers", categoryId: 3))
+    db.session.add(Tag(name: "Confidence building", categoryId: 4))
+    db.session.add(Tag(name: "Public speaking", categoryId: 4))
+    db.session.add(Tag(name: "Job interview skills", categoryId: 4))
+    db.session.add(Tag(name: "Negotiation strategies", categoryId: 4))
+    db.session.add(Tag(name: "Goal setting", categoryId: 4))
+    db.session.add(Tag(name: "Professional networking", categoryId: 4))
+    db.session.add(Tag(name: "Leadership skills", categoryId: 4))
+    db.session.add(Tag(name: "Getting on boards", categoryId: 4))
+    db.session.add(Tag(name: "Communication skills", categoryId: 4))
+    db.session.add(Tag(name: "Managing teams", categoryId: 4))
+    db.session.add(Tag(name: "Building a personal brand", categoryId: 4))
+    db.session.add(Tag(name: "Work/Life Balance", categoryId: 5))
 
 # Uses a raw SQL query to TRUNCATE the tags table.
 # SQLAlchemy doesn't have a built in function to do this
