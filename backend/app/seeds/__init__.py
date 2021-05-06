3dbfd07d9e71235c42cee4dfe592c501c3822b9b
junaid <junaidmaknojia786@yahoo.com>
2021-05-06 01:27:31 -0500

    ready to seed

--- backend/app/seeds/__init__.py
+++ backend/app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
-from .tags import seed_tags, undo_tags
+from .tags import seed_tags, undo_tags, seed_tag_categories, undo_tag_categories
+from .attributes import seed_industries, seed_role_categories, seed_roles, undo_industries, undo_role_categories, undo_roles
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -9,11 +10,19 @@ seed_commands = AppGroup('seed')
 # Creates the `flask seed all` command
 @seed_commands.command('all')
 def seed():
+    seed_role_categories()
+    seed_tag_categories()
+    seed_roles()
+    seed_tags()
+    seed_industries()
     seed_users()
-    # Add other seed functions here
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_industries()
     undo_users()
-    # Add other undo functions here
+    undo_tags()
+    undo_roles()
+    undo_role_categories()
+    undo_tag_categories()
