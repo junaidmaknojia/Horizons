574a85ef304e95e7eae852f9856d22c12f399f11
junaid <junaidmaknojia786@yahoo.com>
2021-05-09 23:01:31 -0500

    solved duplicate problem with overwriting object

--- frontend/src/components/BrowseMentors/index.js
+++ frontend/src/components/BrowseMentors/index.js
@@ -27,9 +27,26 @@ export default function BrowseMentors() {
     }, [dispatch]);
 
     useEffect(() => {
-        const tempMentors = new Set([...addedTags.map(tag => tag.users)].flat());
-        console.log(Array.from(tempMentors));
-        setListMentors([...tempMentors].flat());
+        // const tempMentors = new Set([...addedTags.map(tag => tag.users)].flat());
+        const temp = [...addedTags.map(tag => tag.users)].flat();
+        const tempObj = {}
+        temp.forEach(user => {
+            tempObj[user.id] = user;
+        });
+        console.log(Object.values(tempObj));
+        // console.log(Array.from(tempMentors));
+        // setListMentors([...tempMentors].flat());
+
+        // const array = [{...},{...},{...},{...},...];
+        // const result = [];
+        // const map = new Map();
+        // for (const item of array) {
+        //     if(!map.has(item.id)){
+        //         map.set(item.id, true);    // set any value to Map
+        //         result.push({id: item.id, name: item.name});
+        //     }
+        // }
+
     }, [addedTags]);
 
     function updateTags(action, tag){
