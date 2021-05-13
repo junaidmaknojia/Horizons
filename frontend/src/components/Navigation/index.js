45ca3a96a66695dec4ec373cb8821d6c93f794b4
junaid <junaidmaknojia786@yahoo.com>
2021-05-13 14:26:21 -0500

    refactoring navbar

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -5,23 +5,40 @@ import "./Navigation.css";
 import { NavLink } from "react-router-dom";
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal";
-import { Nav } from 'react-bootstrap';
+import { Modal } from "../../context/Modal";
+import SignupForm from "./SignupForm";
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
 
 export default function Navigation({ isLoaded }) {
+
     const sessionUser = useSelector(state => state.session.user);
 
-    let sessionLinks;
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
+    const [showSignUp, setShowSignUp] = useState(false);
 
-    if (sessionUser) {
-        sessionLinks = (<ProfileButton user={sessionUser} />);
-    } else {
-        sessionLinks = (
-            <>
-                <LoginFormModal/>
-                <SignupFormModal/>
-            </>
-        );
+    function openMenu() {
+        if (!showMenu) setShowMenu(true);
     }
+
+    useEffect(() => {
+        if (!showMenu) return;
+
+        const closeMenu = () => {
+            setShowMenu(false);
+        };
+
+        document.addEventListener('click', closeMenu);
+
+        return () => document.removeEventListener("click", closeMenu);
+    }, [showMenu]);
+
+    const logout = (e) => {
+        e.preventDefault();
+        dispatch(sessionActions.logout());
+        history.push("/");
+    };
 
     return (
         // <div className="navBar">
@@ -30,19 +47,28 @@ export default function Navigation({ isLoaded }) {
         //     {isLoaded && sessionLinks}
         // </div>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Horizons</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link onClick={() => setShowSignUp(true)}>Get Started
+                {showSignUp && (
+                    <Modal onClose={() => setShowSignUp(false)}>
+                        <SignupForm/>
+                    </Modal>
+                )}
+            </Nav.Link>
+            <Nav.Link href="#pricing">{isLoaded && sessionLinks}</Nav.Link>
+
+            {sessionUser && (
+                <NavDropdown title="Profile" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="/edit">Edit Profile</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Log Out</NavDropdown.Item>
+                {/* <NavDropdown.Item href="#action/3.3">S</NavDropdown.Item> */}
+                {/* <NavDropdown.Divider /> */}
+                {/* <NavDropdown.Item href="#action/3.4"></NavDropdown.Item> */}
+                </NavDropdown>
+            )}
           </Nav>
           <Nav>
             <Nav.Link href="#deets">More deets</Nav.Link>
