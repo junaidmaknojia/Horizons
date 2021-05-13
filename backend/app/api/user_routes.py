f1022708b6a264dec7d4b8fcc59503b8bb16edcf
junaid <junaidmaknojia786@yahoo.com>
2021-05-13 01:14:24 -0500

    about to test file upload

--- backend/app/api/user_routes.py
+++ backend/app/api/user_routes.py
@@ -17,7 +17,6 @@ def users():
 @user_routes.route('/<int:id>')
 @login_required
 def user(id):
-    print("-------- inside get user")
     user = User.query.get(id)
     return user.to_dict()
 
@@ -39,34 +38,33 @@ def update_user():
         tag = Tag.query.get(tagId)
         user.tags.append(tag)
     db.session.commit()
-
-    # if "image" not in request.files:
-    #     return {"errors": "image required"}, 400
-
-    # image = request.files["image"]
-
-    # if not allowed_file(image.filename):
-    #     return {"errors": "file type not permitted"}, 400
-
-    # image.filename = get_unique_filename(image.filename)
-
-    # upload = upload_file_to_s3(image)
-
-    # if "url" not in upload:
-    #     # if the dictionary doesn't have a url key
-    #     # it means that there was an error when we tried to upload
-    #     # so we send back that error message
-    #     return upload, 400
-
-    # url = upload["url"]
-    # # flask_login allows us to get the current user from the request
-    # new_image = Image(user=current_user, url=url)
-    # db.session.add(new_image)
-    # db.session.commit()
-    # return {"url": url}
-
     return user.to_dict()
 
+@user_routes.route("/image/", methods=["PATCH"])
+def update_profile_photo():
+    if "image" not in request.files:
+        return {"errors": "image required"}, 400
+
+    image = request.files["image"]
+
+    if not allowed_file(image.filename):
+        return {"errors": "file type not permitted"}, 400
+
+    image.filename = get_unique_filename(image.filename)
+
+    upload = upload_file_to_s3(image)
+
+    if "url" not in upload:
+        # if the dictionary doesn't have a url key
+        # it means that there was an error when we tried to upload
+        # so we send back that error message
+        return upload, 400
+
+    url = upload["url"]
+    # flask_login allows us to get the current user from the request
+    current_user["profile_photo"] = url
+    db.session.commit()
+    return current_user.to_dict()
 
 @user_routes.route("/mentors/")
 def get_all_mentors():
