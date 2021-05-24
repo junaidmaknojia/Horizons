d821a7850f39a93d66e79f3bc6874a7097baa7c2
junaid <junaidmaknojia786@yahoo.com>
2021-05-24 01:47:26 -0500

    made footer, getting it stay at bottom of page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import LinkedInLogIn from "./components/LinkedInLogIn";
 import Members from "./components/Members";
 import GoogleSignUp from "./components/GoogleSignUp";
 import GoogleLogIn from "./components/GoogleLogIn";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -66,6 +67,7 @@ function App() {
 					<Route><h1>The page you're looking for doesn't exist</h1></Route>
 				</Switch>
 			)}
+			<Footer/>
 		</>
 	);
 }
